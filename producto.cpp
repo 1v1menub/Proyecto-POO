35318d7db231f3fcab68905324ce365f7ecae4cd
lender512 <52045791+lender512@users.noreply.github.com>
2019-11-01 22:03:16 -0500

    Update producto.cpp

--- producto.cpp
+++ producto.cpp
@@ -5,14 +5,13 @@
 #include "producto.h"
 
 //constructor para inicializar los atributos
-Producto::Producto(letra nom) {
-    nombre = nom;
+Producto::Producto(nombre nom) {
+    n = nom;
 }
 
 //retornar el nombre
-letra Producto::get_nombre() {
-
-    return nombre;
+nombre Producto::get_nombre() {
+    return n;
 }
 
 
