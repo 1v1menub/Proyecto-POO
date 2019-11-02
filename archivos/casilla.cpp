10d56c4ff270c8fa170ee260b50d3173b101e15e
lender512 <52045791+lender512@users.noreply.github.com>
2019-11-01 20:52:05 -0500

    Update casilla.cpp

--- archivos/casilla.cpp
+++ archivos/casilla.cpp
@@ -1,24 +1,24 @@
 #include "casilla.h"
 
-casilla::casilla(longitud h, longitud w, coor x, coor y) {
+Casilla::Casilla(longitud h, longitud w, coor x, coor y) {
 	height = h;
 	width = w;
 	posicion[0] = x;
 	posicion[1] = y;
 	lleno = 0;
 }
-void casilla::set_estado(estado valor) {
+void Casilla::set_estado(estado valor) {
 	//False si no esta lleno True si si lo esta
 	lleno = valor;
 }
-estado casilla::get_estado() {
+estado Casilla::get_estado() {
 	return lleno;
 }
 
-longitud casilla::get_width() {
+longitud Casilla::get_width() {
 	return width;
 }
 
-longitud casilla::get_height() {
+longitud Casilla::get_height() {
 	return height;
 }
