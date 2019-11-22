0d30229f1db607201fcb3e98c257dfc0acfb1e9d
mau23102001 <56857213+mau23102001@users.noreply.github.com>
2019-11-22 12:45:27 -0500

    Update main.cpp

--- archivos/main.cpp
+++ archivos/main.cpp
@@ -1,1 +1,37 @@
-#proximamente
+//proximamente :'v
+//combalidaciones cin cout bienvenido numero de casilla,etc
+#include "casilla.h"
+#include "producto.h"
+#include "robot.h"
+#include "tipos.h"
+#include "slot.h"
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    //creacion del objeto -- en progreso
+    //combalidaciones
+    int numero_robots,filas,columnas;
+    int x,y;
+    //bienvenida al programa
+    cout<<"Bienvenido!"<<endl;
+    cout<<"------------------------"<<endl;
+    cout<<"Ingrese el numero de filas :";
+    cin>>filas;
+    cout<<"Ingrese el numero de columnas: ";
+    //-->creacion de la casilla
+    cin>>columnas;
+    //numero de robots
+    cout<<"Ingrese el numero de robots: ";
+    cin>>numero_robots;
+    //estableciendo la posicion de de cada uno de los robots
+    for(int i=0; i<numero_robots; i++)
+    {
+        cout<<"Ingrese el posicion x del robot: ";
+        cin>>x;
+        cout<<"Ingrese la posicion y del robot: ";
+        cin>>y;
+    }
+    //para que se pueda realizar las acciones es necesario haber creado al objeto(en pogreso)
+
