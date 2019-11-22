ef2b24bc5c568e66d57caf21324bf1a094873611
1v1menub <52574898+1v1menub@users.noreply.github.com>
2019-11-22 13:07:41 -0500

    Update slot.h

--- archivos/slot.h
+++ archivos/slot.h
@@ -1,3 +1,7 @@
+#ifdef SLOT_H
+#define SLOT_H
+
+
 #include <vector>
 #include <string>
 #include "tipos.h"
@@ -8,15 +12,19 @@ using namespace std;
 class Slot {
   private:
     numero numero_slot;
-    vector<coor> posicion_slot
+    vector<coor> posicion_slot;
     cantidad capacidad;
     estado estado_slot;
-    vector<Producto> productos;
+    vector<Producto*> productos;
   public:
-    Slot(numero numero_slot, vector<coor> posicion_slot, cantidad capacidad, estado estado_slot, vector<Producto> productos);
-    void agregar_producto(Producto producto);
-    void quitar_producto(nombre nombre_producto);
+    Slot(numero numero_slot, vector<coor> posicion_slot, cantidad capacidad, estado estado_slot, vector<Producto*> productos);
+    void agregar_producto(Producto* producto);
+    void quitar_producto(Producto* producto);
+    vector<Producto*> get_producto();
+    cantidad get_capacidad();
     estado get_estado_slot();
     numero get_numero_slot();
     vector<coor> get_posicion_slot();
-}
+};
+
+#endif
