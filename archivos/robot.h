55a3739160540b2316755667b28d5a5f97f323b9
lender512 <52045791+lender512@users.noreply.github.com>
2019-11-28 18:01:56 -0500

    Update robot.h

--- archivos/robot.h
+++ archivos/robot.h
@@ -1,31 +1,33 @@
-#ifndef ROBOT_H
-#define ROBOT_H
+#ifndef ROBOT
+#define ROBOT
 
 #include <vector>
 #include <string>
 #include "tipos.h"
-#include "producto.h"
+//#include "producto.h"
 
 using namespace std;
 
 class Robot {
-  private:
-    numero numero_robot;
-    estado estado_robot;
-    vector<coor> posicion_home;
-    vector<coor> posicion_robot;
-    Producto* contenido;
-  public:
-    Robot(numero numero_robot, estado estado_robot, coor x, coor y, vector<coor> posicion_robot, Producto* contenido);
-    void set_Posicion_X(coor x);
-	  void set_Posicion_Y(coor y);
-    void depositar();
-    void cargar(Producto* producto);
-    void esperar();
-    Producto* get_producto();
-    estado get_estado_robot();
-    numero get_numero_robot();
-    vector<coor> get_posicion_robot();
+private:
+	numero_t numero_robot;
+	vector<coor> posicion_robot;
+	vector<coor> home;
+	nombre producto;
+public:
+	Robot(numero_t numero_robot, coor x, coor y); //Producto* contenido);
+	void set_Posicion_X(coor x);
+	void set_Posicion_Y(coor y);
+
+	void depositar();
+	//void cargar(Producto* producto);
+	void esperar();
+	//Producto* get_producto();
+	numero_t get_numero_robot();
+	vector<coor> get_posicion_robot();
+	vector<coor> get_home_robot();
+	void set_producto(nombre prod);
+	nombre get_producto();
 };
 
 #endif
