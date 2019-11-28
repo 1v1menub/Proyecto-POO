11f1c87a7ca4214072c6e1966235a0fde8c22172
lender512 <52045791+lender512@users.noreply.github.com>
2019-11-28 18:01:10 -0500

    Update robot.cpp

--- archivos/robot.cpp
+++ archivos/robot.cpp
@@ -1,12 +1,11 @@
-#include "robot.h"
+#include "Robot.h"
 
-Robot::Robot(numero _numero_robot, estado _estado_robot, coor x, coor y, vector<coor> _posicion_robot, Producto* _contenido) {
-  numero_robot = _numero_robot;
-  estado_robot = _estado_robot;
-  posicion_home = _posicion_home;
-  posicion_robot = {x, y};
-  contenido = _contenido;
+Robot::Robot(numero_t _numero_robot, coor x, coor y) {// Producto* _contenido) {
+	numero_robot = _numero_robot;
+	posicion_robot = { x, y };
+	home = { x, y };
 }
+
 
 void Robot::set_Posicion_X(coor x) {
 	posicion_robot[0] = x;
@@ -16,33 +15,24 @@ void Robot::set_Posicion_Y(coor y) {
 	posicion_robot[1] = y;
 }
 
-void Robot::depositar() {
-  estado_robot = false;
-  contenido = nullptr;
-}
 
-void Robot::cargar(Producto* producto) {
-  estado_robot = true;
-  contenido = producto;
-}
 
-void Robot::esperar() {
-  posicion_robot[0] = posicion_robot[0];
-  posicion_robot[1] = posicion_robot[1]; 
-} // ya se que esto no hace nada pero como tengo imaginado la cosa se le otorga 1 movimiento a cada robot y luego se ejecutan todos y luego otro y asi, si la instruccion detecta una colision entonces le dice al robot que ejecute la funcion esperar. 
-
-Producto* Robot::get_producto() {
-  return contenido;
-}
-
-estado Robot::get_estado_robot() {
-  return estado_robot;
-}
-
-numero Robot::get_numero_robot() {
-  return numero_robot;
+numero_t Robot::get_numero_robot() {
+	return numero_robot;
 }
 
 vector<coor> Robot::get_posicion_robot() {
-  return posicion_robot;
+	return posicion_robot;
+}
+
+void Robot::set_producto(nombre prod) {
+	producto = prod;
+}
+
+nombre Robot::get_producto() {
+	return producto;
+}
+
+vector<coor> Robot::get_home_robot() {
+	return home;
 }
