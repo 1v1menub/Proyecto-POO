252b4f972eaaa4019a1304528f1002e844084241
1v1menub <52574898+1v1menub@users.noreply.github.com>
2019-11-29 10:56:54 -0500

    Update slot.h

--- archivos/slot.h
+++ archivos/slot.h
@@ -16,7 +16,6 @@ private:
 	nombre producto;
 public:
 	Slot(numero_t numero_slot, coor x, coor y);
-	cantidad get_capacidad();
 	numero_t get_numero_slot();
 	vector<coor> get_posicion_slot();
 	void set_pruducto(nombre prod);
